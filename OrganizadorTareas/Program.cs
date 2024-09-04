901dfe80b5715f71e491776e63a6b0310781b73f
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-09-04 09:09:29 -0600

    se crea el menu y la funcion para agregar las tareas

--- OrganizadorTareas/Program.cs
+++ OrganizadorTareas/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OrganizadorTareas;
+
+List <Tarea> Tareas = new List<Tarea>();
+do
+{
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("╔══════════════════════════╗");
+    Console.WriteLine("║ ORGANIZADOR DE TAREAS CH ║");
+    Console.WriteLine("╠══════════════════════════╣");
+    Console.WriteLine("║ SELECCIONE UNA OPCION:   ║");
+    Console.WriteLine("║       1. Agregar tarea   ║");
+    Console.WriteLine("║       2. Ver tareas      ║");
+    Console.WriteLine("║       3. Completar tarea ║");
+    Console.WriteLine("╚══════════════════════════╝");
+    Console.ResetColor();
+    Console.Write("\nIngrese un numero para continuar a dicho menu: ");
+    string option = Console.ReadLine().Trim();
+    switch (option)
+    {
+        case "1":
+            AgregarTarea();
+    }
+} while(true);
+
+void AgregarTarea()
+{
+    string nombre = null, descripcion = null;
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("╔══════════════════════════╗");
+    Console.WriteLine("║       AGREGAR TAREA      ║");
+    Console.WriteLine("╚══════════════════════════╝\n");
+    (int left, int top) = Console.GetCursorPosition();
+    do
+    {
+        Console.SetCursorPosition(left, top);
+        Console.Write("Ingrese Nombre tarea: ");
+        nombre = Console.ReadLine().ToUpper().TrimEnd();
+    } while (nombre is null || nombre.Length == 0);
+    (left,top) = Console.GetCursorPosition();
+    do
+    {
+        Console.SetCursorPosition(left, top);
+        Console.Write("Ingrese Descripcion: ");
+        descripcion = Console.ReadLine().TrimEnd();
+    } while (descripcion is null || descripcion.Length == 0);
+
+}
