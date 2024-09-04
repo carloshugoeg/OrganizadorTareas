6565b66dcbf9f76c2c240a00075a5c3d2485ed49
Carlos Hugo Escobar Gomez <chescobarg@correo.url.edu.gt>
2024-09-04 08:51:16 -0600

    Agregar archivos de proyecto.

---
+++ OrganizadorTareas/Tarea.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganizadorTareas
+{
+    internal class Tarea
+    {
+        public Tarea(string nombre, string descripcion)
+        {
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Completada = false;
+        }
+
+        private string Nombre { get; set; }
+        private string Descripcion { get; set; }
+        private bool Completada { get; set; }
+
+        public void MostrarInfo()
+        {
+            Console.WriteLine("--------TAREA---------");
+            Console.WriteLine("\nNombre: " + Nombre);
+            Console.WriteLine("\nDescripcion: " +  Descripcion);
+            Console.Write("\nCompletada: ");
+            if (Completada) Console.WriteLine("Si");
+            else Console.WriteLine("No");
+        }
+        
+        public void CompletarTarea(bool completada) => Completada = completada;
+    }
+}
